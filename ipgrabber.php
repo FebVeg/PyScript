aa514156846feca2cab6396be451fb7ce2438508
FebVeg <feb19958@gmail.com>
2020-06-10 22:00:48 +0000

    A patetic ip grabber
    A patetic ip grabber

---
+++ ipgrabber.php
@@ -0,0 +1,11 @@
+<?php
+  echo "A patetic ip logger in php.. yeah.. that's right dude... I know..";
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$browser = $_SERVER['HTTP_USER_AGENT'];
+	$dateTime = date('Y/m/d G:i:s');
+	$file = "ip.html";
+	$file = fopen($file, "a");
+	$data = "<pre><b>User IP</b>: $ip <b> Browser</b>: $browser <br><b>Time: </b>$dateTime <br></pre>";
+	fwrite($file, $data);
+	fclose($file);
+?> 
